e0f7d416ae4d47d0a08623adad81279d4488c65b
Kriptikz <Kriptikz@users.noreply.github.com>
2018-09-29 15:44:05 -0500

    Setup input for our RightClick key binding in our TDMPlayerController.
    Added setup for handing when the player presses and releases the "RightClick" key binding which is Right Mouse Button by default. Input is only handled on the client so on Tick we will check if the right mouse button is down and do a RPC for the server to handle it.

--- Source/TopDownMultiplayer/Private/TDMPlayerController.cpp
+++ Source/TopDownMultiplayer/Private/TDMPlayerController.cpp
@@ -5,3 +5,36 @@
 
 
 
+ATDMPlayerController::ATDMPlayerController()
+{
+	bRightMouseDown = false;
+}
+
+void ATDMPlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (bRightMouseDown)
+	{
+		// Tell the server we right clicked and send updated mouse location
+
+	}
+}
+
+void ATDMPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("RightClick", IE_Pressed, this, &ATDMPlayerController::RightMousePressed);
+	InputComponent->BindAction("RightClick", IE_Released, this, &ATDMPlayerController::RightMouseReleased);
+}
+
+void ATDMPlayerController::RightMousePressed()
+{
+	bRightMouseDown = true;
+}
+
+void ATDMPlayerController::RightMouseReleased()
+{
+	bRightMouseDown = false;
+}
--- Source/TopDownMultiplayer/Public/TDMPlayerController.h
+++ Source/TopDownMultiplayer/Public/TDMPlayerController.h
@@ -7,14 +7,37 @@
 #include "TDMPlayerController.generated.h"
 
 /**
- * 
+ * This is our base player controller. All input will be routed through here where the inputs function will send an RPC to the server side
+ * version of this controller where it will handle the players input. This will mean there is very little client prediction so what you see 
+ * is what is happening on the server but this also means there will be some input lag based on ping. 
  */
 UCLASS()
 class TOPDOWNMULTIPLAYER_API ATDMPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+
+	ATDMPlayerController();
 	
-	
-	
+
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+
+	/** A toggle for handling when the right mouse button is held down, checked on tick. */
+	UPROPERTY(BlueprintReadOnly, Category = "Input")
+	bool bRightMouseDown;
+
+	/** This is where we setup our input delegates. These will call local functions which will send an RPC to call the server side version. */
+	virtual void SetupInputComponent() override;
+
+	/** Client function delegate for when the right mouse button is pressed, it will toggle our bool bRightMouseDown to true where it is checked on tick. */
+	UFUNCTION()
+	void RightMousePressed();
+
+	/** Client function delegate for when the right mouse button is released, it will toggle our bool bRightMouseDown to false where it is checked on tick. */
+	UFUNCTION()
+	void RightMouseReleased();
+
 };
