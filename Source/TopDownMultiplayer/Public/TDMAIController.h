4a7dcc93e1097dfca3e3131deba7dc096df644e8
Kriptikz <Kriptikz@users.noreply.github.com>
2020-04-15 12:49:44 -0500

    Added an AIController

---
+++ Source/TopDownMultiplayer/Public/TDMAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "TDMAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNMULTIPLAYER_API ATDMAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
